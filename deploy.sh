0c53284316982da37a8c312c830dc75413aeb7f4
ArthurHoaro <arthur@hoa.ro>
2016-12-21 15:27:07 +0100

    Travis: Launch aglio compilation on PR to raise syntax errors

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,6 @@ function doCompile {
     aglio -i api-documentation.md -o out/index.html
 }
 
-# Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
-    exit 0
-fi
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
@@ -33,6 +27,12 @@ rm -rf out/* || exit 0
 
 # Run our compile script
 doCompile
+
+# Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
 
 # Now let's go have some fun with the cloned repo
 cd out
